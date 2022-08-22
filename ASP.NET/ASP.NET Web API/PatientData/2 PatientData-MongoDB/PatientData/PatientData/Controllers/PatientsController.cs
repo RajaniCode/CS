df80fe47f4b7bc02952ecc0aa839d2a43e4d5319
Rajani <rajaniapple@Rajanis-MacBook-Pro.local>
2022-08-22 11:15:52 +0530

    Adding C#, .NET, .NET Core, ASP.NET Core MVC, ASP.NET Core Razor Pages, Entity Framework Core

---
+++ ASP.NET/ASP.NET Web API/PatientData/2 PatientData-MongoDB/PatientData/PatientData/Controllers/PatientsController.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PatientData.Controllers
+{
+    public class PatientsController : ApiController
+    {
+        // Obsolete
+        // MongoCollection<Patient> _patients;
+        IMongoCollection<Patient> _patients;
+
+        public PatientsController()
+        {
+            _patients = PatientDb.Open();
+        }
+
+        public IEnumerable<Patient> Get()
+        {
+            // Obsolete
+            // return _patients.FindAll();
+            return _patients.Find(_ => true).ToList();
+        }
+    }
+}
